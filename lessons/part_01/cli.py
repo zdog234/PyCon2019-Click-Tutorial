927b8892204c92be43176749d238b22516330b8a
Zane Dufour <zane.dufour@gmail.com>
2019-05-01 10:13:28 -0400

    Finished lessong 3 of tutorial

--- lessons/part_01/cli.py
+++ lessons/part_01/cli.py
@@ -4,8 +4,13 @@ import click
 
 
 @click.command()
-def cli():
-    print("Hello.")
+@click.argument('names', nargs=-1)
+@click.option('--greeting', '-g', default='Hello')
+@click.option('--question', is_flag=True, default=False)
+def cli(greeting, names, question):
+    punc = '?' if question else '!'
+    for name in names:
+        print(f"{greeting}, {name}{punc}")
 
 
 if __name__ == "__main__":
