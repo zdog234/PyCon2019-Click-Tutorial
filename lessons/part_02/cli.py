21ab120a933dccd12ae442396e5e53a4305df631
Zane Dufour <zane.dufour@gmail.com>
2019-05-01 11:40:07 -0400

    **cli.py

--- lessons/part_02/cli.py
+++ lessons/part_02/cli.py
@@ -1,11 +1,38 @@
 #!/usr/bin/env python
 
 import click
+import sys
+import os
 
 
 @click.command()
-def cli():
-    print("Hello.")
+@click.option('--red', is_flag=True, default=False)
+# @click.argument('input-file', type=os.PathLike)
+# @click.argument('output-file', type=os.PathLike)
+@click.argument('input-file', type=click.File('r'), default='-')
+@click.argument('output-file', type=click.File('w'), default='-')
+def cli(red,
+        input_file,
+        output_file,
+        ):
+
+    
+    color = 'red' if red else 'green'
+    def cecho(content, **kwargs):
+        click.secho(content, fg=color, **kwargs)
+
+    # color = 'red' if red else (144, 132, 22)
+    cecho("Hello!")
+    cecho("Printing...", err=True)
+
+    input_data = input_file.read()
+    cecho(f'Input length: {len(input_data)}', err=True)
+    if input_file:
+        cecho(input_data,
+              file=output_file, # will default to stdout
+              nl=False, # Don't add newline to the end
+              )
+
 
 
 if __name__ == "__main__":
